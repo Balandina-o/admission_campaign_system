aeee5c7f6f8aca3fd946b9e5eba064c5b1c438ce
balandina-o <balandina.olga.2021@gmail.com>
2024-03-22 02:41:24 +0500

    Заполнение заголовка. Добавление наименования спецаильности

--- src/pages/Protocol.jsx
+++ src/pages/Protocol.jsx
@@ -1,9 +1,16 @@
 import React, { useContext, useEffect } from 'react'
 import { Context } from "../App";
+import { useState } from 'react';
+import "../pages/styles/ProtocolStyle.css";
 
 const Protocol = () => {
     const { protocolParametersFromStore } = useContext(Context);
-    const { statementsFromStore } = useContext(Context);
+    // const { statementsFromStore } = useContext(Context);
+    const { specialitiesFromStore } = useContext(Context);
+    const [specNameFromId, setSpecNameFromId] = useState("");
+    const [currentSpecId, setCurrentSpecId] = useState("");
+
+
     const columns = [ //Массив объектов с заголовками столбцов таблицы
         { heading: "ФИО" },
         { heading: "Возраст" },
@@ -13,20 +20,27 @@ const Protocol = () => {
     ]
 
     useEffect(() => void (async () => {
-        // console.log("sss", protocolParametersFromStore.number);
-        console.log(statementsFromStore.findStatementsBySpec(parseInt(protocolParametersFromStore.number)));
-        // console.log(protocolParametersFromStore.number);
+        let currentId = protocolParametersFromStore.speciality;
+        setCurrentSpecId(currentId);
+        setSpecNameFromId(specialitiesFromStore.findSpeciality(currentId).name)
+        console.log(specialitiesFromStore.findSpeciality(protocolParametersFromStore.speciality).name);
+        console.log(currentSpecId);
+
     })(), [])
 
     return (
         <div style={{ background: "white" }}>
-            <table>
-                <thead>
-                    <tr>
-                        {columns.map((item, index) => <th key={index}>{item.heading}</th>)}
-                    </tr>
-                </thead>
-                {/* <tbody>
+            <div className='protocol_header'>
+                Протокол конкурсного отбора {specNameFromId}
+            </div>
+            <div className='protocol_table_div'>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            {columns.map((item, index) => <th key={index}>{item.heading}</th>)}
+                        </tr>
+                    </thead>
+                    {/* <tbody>
                 <>
                     {users.usersList1.map((user) => {
                         return (
@@ -43,7 +57,9 @@ const Protocol = () => {
                     })}
                 </>
             </tbody> */}
-            </table>
+                </table>
+            </div>
+
         </div>
 
     )
